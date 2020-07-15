e586ce3a4cdfca01e3ffd82a7c031450d4963cc1
likejimsaid <32275213+likejimsaid@users.noreply.github.com>
2020-07-15 06:21:37 -0400

    Add files via upload

---
+++ Buttons_Report_v1.jsx
@@ -0,0 +1,86 @@
+﻿
+
+
+//Script authored in 2020 by James McNair
+//Buttons_Report_v1.jsx
+
+
+//Description:
+
+//Generate a tab deliminated report of ALL interactive buttons report
+
+
+
+
+
+
+
+var g = {},
+    d = app.activeDocument,
+    c = 0;
+
+main();
+
+alert("Le rapport (Button_Report.txt) a été créé dans le même dossier que le document InDesign.");
+
+g = null;
+
+
+
+
+
+function main() {
+
+    wipeIt("\r");
+//start the report file
+    logIt("\r" + timeStamp() + "\r");
+    logIt("Document InDesign : " + d.name + "\r\r");
+    logIt("Page :\t\tNom du bouton :\t\tURL du bouton :\r");
+
+//Iterate through buttons in document
+    for (i=0; i<d.buttons.length; i++) {
+        g.myButtonName = d.buttons[i].name;
+        g.myButtonPage = d.buttons[i].parentPage.name;
+        g.myButtonURL = d.buttons[i].gotoURLBehaviors.item(0).url
+        
+        logIt(g.myButtonPage + "\t\t" + g.myButtonName + "\t\t" + g.myButtonURL + "\r");
+    }
+    logIt("Fini.\r\r");
+}
+
+
+
+
+
+function logIt(data) {
+//Create File object
+g.MyReportFile = new File(d.filePath + "/Button_Report.txt");
+g.MyReportFile.encoding = 'UTF-8';
+g.MyReportFile.open('a');
+g.MyReportFile.write(data);
+g.MyReportFile.close();
+}
+
+
+function timeStamp(){
+    g.date = new Date();
+    g.time = g.date.toLocaleString();
+    return g.time;
+}
+
+
+
+
+function wipeIt(data) {
+//Create File object
+g.MyReportFile = new File(d.filePath + "/Button_Report.txt");
+g.MyReportFile.encoding = 'UTF-8';
+g.MyReportFile.open('w');
+g.MyReportFile.write(data);
+g.MyReportFile.close();
+}
+
+
+
+
+
---
+++ Nettoyage_des_noms_v1.jsx
@@ -0,0 +1,43 @@
+﻿//Script authored in 2020 by James McNair
+//NameField_Repair.jsx
+
+
+//Description:
+
+//Search inDesign buttons name fields for series of digits + "-url"
+
+//remove any text in the name field after the url
+
+
+
+
+
+
+var d = app.activeDocument,
+    c = 0;
+
+main();
+
+alert(c + " noms nettoyés.");
+
+g = null;
+
+
+
+
+
+function main() {
+
+    //Iterate through buttons in document
+    for (i = 0; i < d.buttons.length; i++) {
+
+        //Make sure the document button name contains series of digits followed by -url
+        //if it does, check for more after the url and delete
+        if (/^\d+-url.+/.test(d.buttons[i].name)) {
+            myNewButtonName = d.buttons[i].name.replace(/(^\d+-url)(.+)/g, '$1');
+            d.buttons[i].name = myNewButtonName;
+            c++;
+            }
+        //}
+    }
+}
